88e13860484c4b074350a73e9883c0d03865487c
jelle <jelle@192.168.1.100>
2019-05-12 22:56:33 +0200

    added connection info

--- src/main/java/be/mentoringsystems/blockchain/BlockchainApplication.java
+++ src/main/java/be/mentoringsystems/blockchain/BlockchainApplication.java
@@ -3,17 +3,11 @@ package be.mentoringsystems.blockchain;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 public class BlockchainApplication {
     
-    @RequestMapping("/")
-	String home() {
-		return "Hello World!";
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(BlockchainApplication.class, args);
 	}
